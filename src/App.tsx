5a1a96710bdaf076c54775107f9a2b3fa640a32f
Ulrich <ulrichtonmoy1@gmail.com>
2024-01-16 18:51:34 +0600

    :art: padding right added to sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import {
 } from "./components";
 
 function App() {
-
   const contentContainerRef = useRef<HTMLDivElement>(null);
 
   const resetScroll = () => {
@@ -21,7 +20,7 @@ function App() {
     <div>
       <TitleBar />
       <RootLayout>
-        <Sidebar className="p-2">
+        <Sidebar className="p-2 pr-1">
           <ActionButtonsLayout className="flex justify-between" />
           <NotePreviewList
             className="h-[95%] px-2 mt-1 space-y-2 overflow-auto"

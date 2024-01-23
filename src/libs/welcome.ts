9d20f469c4f617af2946d5d8a1220ea12cab2f27
Ulrich <ulrichtonmoy1@gmail.com>
2024-01-24 03:48:13 +0600

    welcome text updated

--- src/libs/welcome.ts
+++ src/libs/welcome.ts
@@ -1,9 +1,12 @@
+const inline_code = "`";
+const code_block = "```";
+
 export const welcome = `
 > If you want your new notes to be empty uncheck 'add welcome content' from settings.
 
-## Welcome to NoteMark 👋🏻
+## Welcome to Obsidian 👋🏻
 
-NoteMark is a simple **note-taking app** that uses **Markdown** syntax to format your notes.
+Obsidian is a simple **note-taking app** that uses **Markdown** syntax to format your notes.
 
 You can create your first note by clicking on the top-left icon on the sidebar, or delete one by clicking on top right icon.
 
@@ -55,4 +58,14 @@ Code block snippets using the following syntax _\`\`\`js\`\`\`_ are **_not suppo
 ### Links
 
 Links are **_not supported_** yet!
+
+# Supported shortcuts
+
+- Use one to six # characters to create a heading. The number of # characters determines the heading level.
+- Use * or - to create a list item.
+- Use > to create a block quote.
+- Select a text and press Ctrl+B to make it bold, Ctrl+I to make it italic, or Ctrl+U to underline it. Use Cmd on macOS.
+- With text selected, use Cmd+K to open the link dialog.
+- Use ${inline_code} to create inline code.
+- Type ${code_block}$lang (with $lang being any supported language, followed by space) to insert a code block.
 `;

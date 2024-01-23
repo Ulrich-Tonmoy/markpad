7a51bb11aaf7243b0f7bbaa9e6726dd108040034
Ulrich <ulrichtonmoy1@gmail.com>
2024-01-24 02:41:31 +0600

    welcome content added to constant

--- src/libs/constant.ts
+++ src/libs/constant.ts
@@ -1,3 +1,6 @@
+import { welcome } from "./welcome";
+
 export const CONFIG_FILE_NAME = `Obsidian/obsidian.config.json`;
 export const AUTO_SAVING_TIME = 3000;
 export const FILE_EXTENSION = ".md";
+export const WELCOME_CONTENT = welcome;

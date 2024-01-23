2adf1e104e03765885c5dab54680c39bc1b17cb8
Ulrich <ulrichtonmoy1@gmail.com>
2024-01-24 03:24:34 +0600

    cleanup

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,5 @@
 import {
   MDXEditor,
-  codeBlockPlugin,
   headingsPlugin,
   imagePlugin,
   linkPlugin,
@@ -30,7 +29,6 @@ export const Editor = () => {
         linkPlugin(),
         imagePlugin(),
         tablePlugin(),
-        codeBlockPlugin(),
         markdownShortcutPlugin(),
       ]}
       contentEditableClassName="outline-none min-h-screen max-w-none text-lg px-4 py-2.5 caret-cursor prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"

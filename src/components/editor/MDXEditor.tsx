0f21b97dd2d20e68f6efede5e3705b281610d208
Ulrich <ulrichtonmoy1@gmail.com>
2024-02-22 22:18:08 +0600

    💄editor height fixed

--- src/components/editor/MDXEditor.tsx
+++ src/components/editor/MDXEditor.tsx
@@ -31,7 +31,7 @@ export const MDXEditor = () => {
         tablePlugin(),
         markdownShortcutPlugin(),
       ]}
-      contentEditableClassName="outline-none min-h-screen max-w-none md:text-lg text-sm px-4 py-2.5 caret-cursor prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
+      contentEditableClassName="outline-none min-h-[calc(100vh-28px)] max-w-none md:text-lg text-sm px-4 py-2.5 caret-cursor prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
     />
   );
 };

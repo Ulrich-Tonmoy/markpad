aec8cf183569a85b6ba381424edadc54a049a5a2
Ulrich <ulrichtonmoy1@gmail.com>
2024-03-05 16:54:40 +0600

    🩹 key error fix

--- src/components/editor/WelcomePage.tsx
+++ src/components/editor/WelcomePage.tsx
@@ -4,20 +4,17 @@ export const WelcomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full divide-y-[1rem] divide-transparent">
       <img src="/logo.png" alt="Markpad" className="size-60" />
-      {ShortcutKeys.map((shortcut, index) => (
-        <div className="grid content-center grid-cols-2 text-gray-400" key={index}>
+      {ShortcutKeys.map((shortcut, i) => (
+        <div className="grid content-center grid-cols-2 text-gray-400" key={i}>
           <span>{shortcut.name}</span>
           <span>
-            {shortcut.keys.map((key, index) => (
-              <>
-                <span
-                  className="py-1.5 px-2 m-2 text-sm font-medium rounded-md border bg-gray-800 border-gray-600"
-                  key={index}
-                >
+            {shortcut.keys.map((key, j) => (
+              <span key={j}>
+                <span className="py-1.5 px-2 m-2 text-sm font-medium rounded-md border bg-gray-800 border-gray-600">
                   {key}
                 </span>
-                {index < shortcut.keys.length - 1 && "+"}
-              </>
+                {j < shortcut.keys.length - 1 && "+"}
+              </span>
             ))}
           </span>
         </div>
